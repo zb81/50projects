df499df6260298e72e85c5b28e0335dfd7c71e45
CoderZb <zbneversettle@gmail.com>
2022-11-16 11:13:14 +0800

    feat: Random Image Feed

---
+++ random-image-feed/script.js
@@ -0,0 +1,18 @@
+const ctn = document.querySelector('.ctn')
+const URL = 'https://source.unsplash.com/random/'
+
+const rows = 5
+
+for (let i = 0; i < rows * 3; i++) {
+  const img = document.createElement('img')
+  img.src = `${URL}${getRandomSize()}`
+  ctn.appendChild(img)
+}
+
+function getRandomSize() {
+  return `${getRandomNr()}x${getRandomNr()}`
+}
+
+function getRandomNr() {
+  return Math.floor(Math.random() * 10) + 300
+}
